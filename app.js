974877b94936cf8b710f877c17313b0e2c7db07b
cjgetty <cameronjgetty@gmail.com>
2024-02-28 13:35:03 -0500

    init commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// Write a welcome message to the user
+// Explain the rules of the game
+// FUNC computer randomly selects either rock, paper, or scissors
+// FUNC play a single round that contains a player selection and a computer selection
+// Return a message that declares the winner
+
+console.log("Welcome to Rock Paper Scissors!");
+console.log("The rules are simple:");
+console.log("Paper beats rock. Rock beats scissors. Scissors beats paper.");
+console.log("Ready to play?");
